1b95f1e1fdb72e4f729babf1e763c29c24163576
azani ahmad <157575444+azaniahmad@users.noreply.github.com>
2026-05-21 22:37:08 +0700

    Update MainTokoBuku.java

--- MainTokoBuku.java
+++ MainTokoBuku.java
@@ -4,18 +4,17 @@
  */
 package maintokobuku;
 
-// 3. MAIN CLASS (Hanya ini yang boleh 'public')
+
 public class MainTokoBuku {
     public static void main(String[] args) {
         
-        // Menggunakan Try-With-Resources (Scanner diletakkan di dalam kurung try)
+      
         try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
             
             BukuToko[] daftarBuku = new BukuToko[3]; 
 
             System.out.println("=== APLIKASI ENTRI DATA TOKO BUKU ===");
             
-            // Proses Input Data Sebanyak 3 Kali
             for (int i = 0; i < 3; i++) {
                 System.out.println("\n--- Input Data Buku ke-" + (i + 1) + " ---");
                 
@@ -34,11 +33,10 @@ public class MainTokoBuku {
                 System.out.print("Kategori (su/r/d/a) : ");
                 String kat = scanner.nextLine();
                 
-                // Instansiasi objek child class
+
                 daftarBuku[i] = new BukuToko(judul, pengarang, penerbit, tahun, kat);
             }
 
-            // Proses Menampilkan Isi Buku
             System.out.println("\n\n=== DAFTAR ISI BUKU TOKO ===");
             System.out.println("=========================================");
             for (int i = 0; i < 3; i++) {
@@ -47,9 +45,9 @@ public class MainTokoBuku {
             }
             
         } catch (Exception e) {
-            // Menangkap error jika terjadi kesalahan input (misal: tahun cetak diisi huruf)
+           
             System.out.println("Terjadi kesalahan input: " + e.getMessage());
         }
-        // TIDAK PERLU lagi menulis scanner.close(); di sini, karena sudah ditutup otomatis oleh try()
+       
     }
 }

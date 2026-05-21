841716a2d2e98cca056a5fb70e8e38305da6d159
azani ahmad <157575444+azaniahmad@users.noreply.github.com>
2026-05-21 22:38:53 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,17 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package maintokobuku;
 
 public class Buku {
-    // Atribut parent class
+
     protected String judul;
     protected String nama_pengarang;
     protected String penerbit;
     protected int tahun_cetak;
 
-    // Constructor
+
     public Buku(String judul, String nama_pengarang, String penerbit, int tahun_cetak) {
         this.judul = judul;
         this.nama_pengarang = nama_pengarang;
@@ -19,7 +16,6 @@ public class Buku {
         this.tahun_cetak = tahun_cetak;
     }
 
-    // Method untuk menampilkan informasi
     public void displayInfo() {
         System.out.println("Judul Buku    : " + judul);
         System.out.println("Pengarang     : " + nama_pengarang);
